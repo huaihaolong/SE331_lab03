239412d33754b419118c8b4b539ee6850215126a
HaolongHuai <1612907625@qq.com>
2022-08-12 08:27:57 +0800

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/event-caor">Category And Organizer</router-link> |
+    <router-link to="student-info">Student Information</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
